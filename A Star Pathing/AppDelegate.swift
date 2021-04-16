2b30d7eae1d1652edb0799489000fb914a017ff8
a1cd <71281043+a1cd@users.noreply.github.com>
2021-04-16 17:03:39 -0400

    Initial Commit

---
+++ A Star Pathing/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  A Star Pathing
+//
+//  Created by Everett Wilber on 4/16/21.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
